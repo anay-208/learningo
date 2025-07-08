b893048cd793f4f4b01974a9075d7c58179392ef
anay-208 <me@anayparaswani.dev>
2025-07-09 01:13:33 +0800

    add another section tohero page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "@/components/home/hero-section";
+import TestimonialsSection from "@/components/home/testimonials-section";
 
 export default function LandingPage() {
   return (
     <div className="min-h-screen w-full flex flex-col items-center justify-center px-4 ">
       <HeroSection />
+      <TestimonialsSection />
       {/* Add more sections here as your app grows */}
       <footer className="w-full text-center text-gray-500 py-4">
         <p className="text-sm">Crafted with ❤️ by Anay Paraswani</p>
